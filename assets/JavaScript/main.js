1cf6eac8f7ec81432f315cae97d3708f92a62a7b
matttali09 <matttaliancich09@gmail.com>
2018-11-21 12:05:42 -0500

    put in the logic with newBtn and event prevent to make submit button fully functional

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -10,7 +10,7 @@ var animals = ["cat",
     "ferret",
     "turtle",
     "sugar glider",
-    "chinchilla",
+    "zelda",
     "hedgehog",
     "hermit crab",
     "gerbil",
@@ -21,27 +21,36 @@ var animals = ["cat",
     "seagull",
     "salamander",
     "frog",
-]
+];
 
 // create new buttons div to append to #buttons-div 
 
-for (var i = 0; i< animals.length; i++) {
+for (var i = 0; i < animals.length; i++) {
     var buttonsDiv = $("<button>").attr("class", "animal")
     buttonsDiv.text(animals[i]);
     $("#buttons-div").append(buttonsDiv);
-}
+};
 
+// make buttons initiate ajax request
 $("#buttons-div").on("click", ".animal", function () {
-        searchAnimal($(this).text())
-})
+    searchAnimal($(this).text());
+});
 
+// append new buttons with submit txt area
+$("#submit-button").on("click", function () {
+    event.preventDefault();
+    var newBtn = $("<button>").attr("class", "animal")
+    newBtn.text($("#input-animal").val().trim())
+    $("#buttons-div").append(newBtn);
+    $("#input-animal").val("");
+    console.log("hi")
+});
 
-// Constructing a queryURL using the animal name with 10 results
-
+// Constructing a function ajax using the animal name with 11 results
 function searchAnimal(string) {
-     var xhr = "https://api.giphy.com/v1/gifs/search?q=" +
+    var xhr = "https://api.giphy.com/v1/gifs/search?q=" +
         string + "&api_key=dc6zaTOxFJmzC&limit=11";
-        
+
     // Performing an AJAX request with the queryURL
     $.ajax({
         url: xhr,
@@ -51,37 +60,32 @@ function searchAnimal(string) {
         .then(function (res) {
             console.log(xhr);
             console.log(res);
-            for (i in res.data) { 
+            for (i in res.data) {
 
 
-            // create giphyDiv to hold all results
-            var giphyDiv = $("<div>");
-            // get rating and create a rating p tag
-            var rating = res.data[i].rating;
-            var p = $("<p>").text("Rating: " + string +" " + rating);
+                // create giphyDiv to hold all results
+                var giphyDiv = $("<div>");
+                // get rating and create a rating p tag
+                var rating = res.data[i].rating;
+                var p = $("<p>").text("Rating: " + string + " " + rating);
 
-            // append it to the giphy div
-            giphyDiv.append(p);
+                // append it to the giphy div
+                giphyDiv.append(p);
 
-            // create image div and get the still image to animate/still later
-            var giphyImg = $("<img>").attr("src", res.data[i].images.original_still.url).attr("data-still", res.data[i].images.original_still.url).attr("data-animate", res.data[i].images.original.url).attr("class", "gif").attr("data-state", "still");
-            
-            giphyDiv.append(giphyImg);
-            giphyDiv.attr("class", "container")
+                // create image div and get the still image to animate/still later
+                var giphyImg = $("<img>").attr("src", res.data[i].images.original_still.url).attr("data-still", res.data[i].images.original_still.url).attr("data-animate", res.data[i].images.original.url).attr("class", "gif").attr("data-state", "still");
 
-            // prepend so they show up first on scroll
-            $("#giphys").prepend(giphyDiv)
+                giphyDiv.append(giphyImg);
+                giphyDiv.attr("class", "container");
 
+                // prepend so they show up at top of screen
+                $("#giphys").prepend(giphyDiv);
 
-
-
-
-            // now that im getting the correct animal i will need to pull the queryURL into the ajax request so that i can get all the animals that will be needed using the forloop
             }
         });
 };
 
-$("#giphys").on("click", ".gif", function(){
+$("#giphys").on("click", ".gif", function () {
     var state = $(this).attr("data-state");
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
